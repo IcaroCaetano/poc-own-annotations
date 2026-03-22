0aecf62fbd9d04d05cdb4471600840f4a11de3d6
IcaroCaetano <icaro.caetano@gmail.com>
2026-03-21 22:39:09 -0300

    fear: first commit

---
+++ src/main/java/com/myprojectsjava/poc_own_annotations/PocOwnAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.myprojectsjava.poc_own_annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocOwnAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocOwnAnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojectsjava/poc_own_annotations/PocOwnAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojectsjava.poc_own_annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocOwnAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
