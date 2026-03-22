967d98147a192fbd28aa9bcaae80b577caca9cf6
IcaroCaetano <icaro.caetano@gmail.com>
2026-03-21 23:05:54 -0300

    doc: add documentation

--- src/main/java/com/myprojectsjava/poc_own_annotations/annotations/Important.java
+++ src/main/java/com/myprojectsjava/poc_own_annotations/annotations/Important.java
@@ -2,6 +2,26 @@ package com.myprojectsjava.poc_own_annotations.annotations;
 
 import java.lang.annotation.*;
 
+/**
+ * Analogia simples
+ * class → cria objeto
+ * interface → define contrato
+ * @interface → define metadado
+ * public @interface Note
+ *
+ * @Retention - Até quando essa annotation vai existir
+ * Ex: SOURCE	Só existe no código, some na compilação
+ *     CLASS	Vai até o .class, mas não em runtime
+ *     RUNTIME	Disponível em tempo de execução
+ *
+ * @Target
+ * Onde essa annotation pode ser usada
+ * TYPE    Classes
+ * METHOD	Métodos
+ * FIELD	Atributos
+ * PARAMETER	Parâmetros
+ * CONSTRUCTOR	Construtores
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
 public @interface Important {
