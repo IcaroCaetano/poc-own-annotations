43e25ec3053d1ab8fa5daac3ea9836318cb46ddc
IcaroCaetano <icaro.caetano@gmail.com>
2026-03-21 22:46:06 -0300

    feat: add annotation

---
+++ src/main/java/com/myprojectsjava/poc_own_annotations/model/StudyClass.java
@@ -0,0 +1,16 @@
+package com.myprojectsjava.poc_own_annotations.model;
+
+import com.myprojectsjava.poc_own_annotations.annotations.Important;
+import com.myprojectsjava.poc_own_annotations.annotations.Note;
+
+@Note("Essa classe esta sendo usanda como exemplo para estudar Reflactions")
+public class StudyClass {
+
+    @Note("Usuario Name Field")
+    private String name;
+
+    @Important
+    public void execute() {
+        System.out.println("Executing...");
+    }
+}
