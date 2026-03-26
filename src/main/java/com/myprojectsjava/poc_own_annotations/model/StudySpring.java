a9fa7a04c6f8e2c6916c612f01ff894a52b3ed1e
IcaroCaetano <icaro.caetano@gmail.com>
2026-03-25 22:51:29 -0300

    feat: add validation with annotation

---
+++ src/main/java/com/myprojectsjava/poc_own_annotations/model/StudySpring.java
@@ -0,0 +1,9 @@
+package com.myprojectsjava.poc_own_annotations.model;
+
+import com.myprojectsjava.poc_own_annotations.annotations.NotNull;
+
+public class StudySpring {
+
+    @NotNull
+    String name;
+}
