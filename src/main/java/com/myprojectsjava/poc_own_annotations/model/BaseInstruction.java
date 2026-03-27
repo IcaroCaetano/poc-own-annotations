3e405b6820684fef18f3cc6a216a1ae817190dd0
IcaroCaetano <icaro.caetano@gmail.com>
2026-03-27 09:23:52 -0300

    feat: add inherite annotation

---
+++ src/main/java/com/myprojectsjava/poc_own_annotations/model/BaseInstruction.java
@@ -0,0 +1,6 @@
+package com.myprojectsjava.poc_own_annotations.model;
+
+import com.myprojectsjava.poc_own_annotations.annotations.Activate;
+
+@Activate(value = "Base")
+public class BaseInstruction {}
